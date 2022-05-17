a0c369b9ce02472777758c7cc6a5345b28dadd8e
nbegildayev <nurmukhamedali@gmail.com>
2022-05-17 18:57:32 +0600

    Initial code

--- src/main/resources/static/js/api/categories.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-const categories = Vue.resource('admin/categories{/id}')
-
-export default {
-    add: category => categories.save({}, category),
-    update: category => categories.update({id: category.id}, category),
-    remove: id => categories.remove({id}),
-}
